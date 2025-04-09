f525f1ca2adfba3e651d85de6cac0310f1d226aa
Aaron Kollasch <aaron.kollasch@basecamp-research.com>
2025-04-09 15:12:41 -0400

    disable ts_ls and eslint lsps in nvim

--- nvim/lua/ak/lsp/init.lua
+++ nvim/lua/ak/lsp/init.lua
@@ -3,8 +3,8 @@ local lsp_zero = require("lsp-zero")
 require("mason").setup({})
 require("mason-lspconfig").setup({
     ensure_installed = {
-        "ts_ls",
-        "eslint",
+        -- "ts_ls",
+        -- "eslint",
         "lua_ls",
         "rust_analyzer",
         "basedpyright",
